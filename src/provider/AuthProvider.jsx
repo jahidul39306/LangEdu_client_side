358aec5cd6e8441d8e88a49ce78b3c0f947ed964
jahidul39306 <jahidulislamananto@gmail.com>
2024-12-22 19:21:56 +0600

    registration page complete

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,17 +1,60 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, updateProfile, signInWithPopup } from "firebase/auth";
 
 const GlobalContext = createContext();
 
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+
+    const auth = getAuth(app);
+    const [user, setUser] = useState(auth.currentUser);
+    const [loading, setLoading] = useState(true);
+
+       // variables for google signin login
+       const googleProvider = new GoogleAuthProvider();
+
+    // create user with email
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // update profile
+    const updateUserProfile = (updatedProfile) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, updatedProfile);
+    }
+
+    // google sigin
+    const createUserWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
 
     const globalInfo = {
-
+        createUser,
+        user,
+        setUser,
+        loading,
+        setLoading,
+        updateUserProfile,
+        createUserWithGoogle
     }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, [auth]);
+
     return (
         <GlobalContext.Provider value={globalInfo}>{children}</GlobalContext.Provider>
     );
 };
 
-export {AuthProvider, GlobalContext };
+export { AuthProvider, GlobalContext };
